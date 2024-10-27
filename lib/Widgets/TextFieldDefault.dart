89f1607cc93c5d4c96b4f4bef4d41ac580e3be61
David <daviidserranoo07@gmail.com>
2024-10-27 19:48:45 +0100

    DAVID: Formularios de registrar alumno y profesor terminados

--- lib/Widgets/TextFieldDefault.dart
+++ lib/Widgets/TextFieldDefault.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tarerio/Widgets/InformationModal.dart';
 
 class TextFieldDefault extends StatelessWidget {
-  final String label;
+  final String label, titleInformation, textInformation;
   final TextEditingController controller;
   final Color labelColor;
   final double labelFontSize;
   final double width;
   final EdgeInsets padding;
   final bool obscureText;
+  final bool information;
 
   const TextFieldDefault({
     Key? key,
@@ -18,23 +20,54 @@ class TextFieldDefault extends StatelessWidget {
     this.width = 350.0,
     this.padding = EdgeInsets.zero,
     this.obscureText = false,
+    this.information = false,
+    this.titleInformation = '',
+    this.textInformation = '',
   }) : super(key: key);
+
+  void _showInformationModal(
+      BuildContext context, String title, String content) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return InformationModal(title: title, content: content);
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: padding,
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: labelFontSize,
-              fontWeight: FontWeight.w600,
-              color: labelColor,
+        Row(
+          children: [
+            Padding(
+              padding: padding,
+              child: Text(
+                label,
+                style: TextStyle(
+                  fontSize: labelFontSize,
+                  fontWeight: FontWeight.w600,
+                  color: labelColor,
+                ),
+              ),
             ),
-          ),
+            if (information)
+              Padding(
+                padding: const EdgeInsets.only(left: 10.0, top: 12.0),
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.help,
+                    size: 30.0,
+                    color: Color(0xFF2EC4B6),
+                  ),
+                  onPressed: () {
+                    _showInformationModal(context, titleInformation, textInformation);
+                  },
+                ),
+              ),
+          ],
         ),
         Padding(
           padding: padding,
@@ -46,8 +79,8 @@ class TextFieldDefault extends StatelessWidget {
               decoration: InputDecoration(
                 fillColor: Colors.grey[200],
                 filled: true,
-                contentPadding: const EdgeInsets.symmetric(
-                    vertical: 15, horizontal: 10),
+                contentPadding:
+                    const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
                 enabledBorder: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(15.0),
                   borderSide: const BorderSide(color: Colors.black12),
