2a5c1c61a5ff592b9314a52d7e43c0b3bce8091e
Rafa Luque Pérez-Vico <rafaluperezvico@gmail.com>
2024-11-05 20:41:08 +0100

    Dialog para asignar profesor a Aula

--- lib/Pages/aulas.dart
+++ lib/Pages/aulas.dart
@@ -1,24 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:tarerio/Widgets/AulaCard.dart';
 import 'package:tarerio/Widgets/Navbar.dart';
+
 import 'package:tarerio/API/AulasAPI.dart';
-import 'package:tarerio/API/ProfesoresAPI.dart';
 import 'package:tarerio/Pages/crearAula.dart';
 import 'package:tarerio/Widgets/ErrorModal.dart';
+import 'package:tarerio/API/ProfesoresAPI.dart';
 import 'package:tarerio/Widgets/SuccessModal.dart';
 
 class AulasPage extends StatefulWidget {
   AulasPage({super.key});
 
-
   @override
   _AulasPageState createState() => _AulasPageState();
+/*
+  final List<Map<String, String>> aulas = [
+    {
+      'nombre': 'Aula A1',
+      'imagenUrl':
+          'https://www.colegiolasrosas.es/imgs/galerias/aulas-educacion-primaria/ColegioLasRosas_aulas_educacion_primaria_1.jpg', // Cambia esta URL por la ruta real
+    },
+    {
+      'nombre': 'Aula A2',
+      'imagenUrl':
+          'https://www.colegiolasrosas.es/imgs/galerias/aulas-educacion-primaria/ColegioLasRosas_aulas_educacion_primaria_1.jpg', // Cambia esta URL por la ruta real
+    },
+    // Agrega más aulas según los datos de tu base de datos
+  ];
+*/
 }
 
 class _AulasPageState extends State<AulasPage> {
   List<dynamic> aulas = [];
   List<dynamic> profesores = [];
-  bool isLoadingAulas = true;
+  bool isloadingAulas = true;
   bool isLoadingProfesores = true;
 
   @override
@@ -28,7 +43,7 @@ class _AulasPageState extends State<AulasPage> {
     fetchProfesores();
   }
 
-void _showErrorModal(BuildContext context, String title, String content) {
+  void _showErrorModal(BuildContext context, String title, String content) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -48,14 +63,18 @@ void _showErrorModal(BuildContext context, String title, String content) {
 
   Future<void> fetchAulas() async {
     try {
-      final response = await AulasAPI().obtenerAulas();
+      AulasAPI _api = AulasAPI();
+      final response = await _api.obtenerAulas();
       setState(() {
-        aulas = response;
-        isLoadingAulas = false;
+        aulas = response; // Actualiza la lista de tareas
+        isloadingAulas = false; // Cambia el estado de carga
       });
     } catch (e) {
       print("Error al obtener aulas: $e");
-      setState(() => isLoadingAulas = false);
+      setState(() {
+        isloadingAulas =
+            false; // Cambia el estado de carga incluso si hay un error
+      });
     }
   }
 
@@ -72,13 +91,105 @@ void _showErrorModal(BuildContext context, String title, String content) {
     }
   }
 
+  void _confirmarEliminacion(String id) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmar Eliminación'),
+          content:
+              const Text('¿Estás seguro de que deseas eliminar este Aula?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Cerrar el diálogo
+              },
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.of(context).pop(); // Cerrar el diálogo
+                await _borrarAula(
+                    id); // Llama a la función para eliminar la tarea
+              },
+              child: const Text('Eliminar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _borrarAula(String id) async {
+    try {
+      AulasAPI _api = AulasAPI();
+      await _api.eliminarAula(id); // Llama al método de eliminación
+      setState(() {
+        aulas.removeWhere(
+            (aula) => aula['id'] == id); // Actualiza la lista de tareas
+      });
+      _showSuccessModal(context, "Exito", "Exito al eliminar el aula");
+    } catch (e) {
+      print("Error al eliminar aula: $e");
+      _showErrorModal(context, "Error", "Error al eliminar el aula");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Aulas'),
+      ),
+      body: isloadingAulas
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: aulas.length,
+              itemBuilder: (context, index) {
+                final aula = aulas[index];
+                int idAula = aula['id_aula'];
+                return AulaCard(
+                  claveAula: aula['clave_aula'] ?? "Sin clave",
+                  cupoAula: aula['cupo'] ?? 0,
+                  imagenUrl: 'assets/images/aula.jpg', //aula['imagenUrl']! //
+                  onEdit: () {
+                    // Lógica para editar aula
+                  },
+                  onAssign: () {
+                    mostrarDialogoProfesores(context, idAula);
+                  },
+                  onDelete: () {
+                    _confirmarEliminacion(aula["id_aula"].toString());
+                  },
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CrearAula()),
+          );
+        },
+        child: const Icon(Icons.add),
+        backgroundColor: const Color(0xFF2EC4B6),
+      ),
+      drawer: Navbar(
+        screenIndex: 2,
+        onLogout: () {
+          print("Cerrar sesión");
+        },
+      ),
+    );
+  }
+
   void mostrarDialogoProfesores(BuildContext context, int idAula) {
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text('Seleccionar Profesor'),
-          content: isLoadingProfesores
+          content: isloadingAulas
               ? const Center(child: CircularProgressIndicator())
               : SizedBox(
                   width: double.maxFinite,
@@ -103,12 +214,13 @@ void _showErrorModal(BuildContext context, String title, String content) {
                           title: Text(
                               profesores[index]['nickname'] ?? 'Sin nombre'),
                           onTap: () async {
-                            int idUsuario = profesores[index]['id_usuario'];
+                            int idUsuario = profesores[index][
+                                'id_usuario']; // Convertimos id_usuario a String
                             try {
                               await AulasAPI()
-                                  .asignarProfesorAula(idAula, idUsuario);                
+                                  .asignarProfesorAula(idAula, idUsuario);
                             } catch (e) {
-                              print(e);
+                              print("Error al asignar profesor $e");
                             }
                           },
                         ),
@@ -124,91 +236,6 @@ void _showErrorModal(BuildContext context, String title, String content) {
           ],
         );
       },
-    );
-  }
-
-  void _confirmarEliminacion(String id) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Confirmar Eliminación'),
-          content: const Text('¿Estás seguro de que deseas eliminar este Aula?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Cerrar el diálogo
-              },
-              child: const Text('Cancelar'),
-            ),
-            TextButton(
-              onPressed: () async {
-                Navigator.of(context).pop(); // Cerrar el diálogo
-                await _borrarAula(id); // Llama a la función para eliminar la tarea
-              },
-              child: const Text('Eliminar'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _borrarAula(String id) async {
-    try {
-      AulasAPI _api = AulasAPI();
-      await _api.eliminarAula(id); // Llama al método de eliminación
-      setState(() {
-        aulas.removeWhere((aula) => aula['id'] == id); // Actualiza la lista de tareas
-      });
-      _showSuccessModal(context, "Exito", "Exito al eliminar el aula");
-    } catch (e) {
-      print("Error al eliminar aula: $e");
-      _showErrorModal(context, "Error", "Error al eliminar el aula");
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Aulas'),
-      ),
-      body: isLoading
-        ? Center(child: CircularProgressIndicator())
-        : ListView.builder(
-        itemCount: aulas.length,
-        itemBuilder: (context, index) {
-          final aula = aulas[index];
-          return AulaCard(
-            claveAula: aula['clave_aula'] ?? "Sin clave",
-            cupoAula: aula['cupo'] ?? 0,
-            imagenUrl: 'assets/images/aula.jpg',  //aula['imagenUrl']! //
-            onEdit: () {
-              // Lógica para editar aula
-            },
-            onAssign: () {
-              // Lógica para asignar aula
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => CrearAula(),
-            ),
-          );
-        },
-        child: const Icon(Icons.add),
-        backgroundColor: const Color(0xFF2EC4B6),
-      ),
-      drawer: Navbar(
-        screenIndex: 2,
-        onLogout: () => print("Cerrar sesión"),
-      ),
     );
   }
 }
