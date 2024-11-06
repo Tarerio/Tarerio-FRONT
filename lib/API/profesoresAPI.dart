af4f48f988a68c64f8ae7e50bc1cec099fa00911
David Serrano Domínguez <>
2024-11-06 17:32:52 +0100

    DAVID: cambios

--- lib/API/profesoresAPI.dart
+++ lib/API/profesoresAPI.dart
@@ -1,48 +1,24 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:tarerio/consts.dart';
 
 // API de Profesores
 class ProfesoresAPI {
-  Future<List<dynamic>> obtenerProfesores() async {
-    String url = '$baseUrl/profesores';
+  static const String _baseUrl = 'http://localhost:3000';
 
+  /// Método para obtener todos los profesores
+  Future<List<dynamic>> obtenerProfesores() async {
+    String url = '$_baseUrl/profesores';
+
+    // Realizar la solicitud GET
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
+      // Decodificar la respuesta JSON
       final Map<String, dynamic> data = jsonDecode(response.body);
-      if (data.containsKey('usuarios') && data['usuarios'] != null) {
-        return data['usuarios'];
-      } else {
-        throw Exception('Key "usuarios" not found or is null');
-      }
+      return data['usuarios']; // Retorna la lista de profesores
     } else {
-      throw Exception('Failed to load data');
-    }
-  }
-
-  cambiarContraseniaProfesor(
-      int id, String contraseniaActual, String contraseniaNueva) async {
-    String url = '$baseUrl/profesores/$id/cambiarContrasenia';
-
-    final Map<String, dynamic> data = {
-      "contraseniaActual": contraseniaActual,
-      "contraseniaNueva": contraseniaNueva,
-    };
-
-    final response = await http.put(
-      Uri.parse(url),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: json.encode(data),
-    );
-
-    if (response.statusCode == 201) {
-      return json.decode(response.body);
-    } else {
-      throw Exception('Failed to change password: ' +
-          json.decode(response.body)['message']);
+      // Lanzar una excepción si hay un error
+      throw Exception('Error al cargar los profesores: ${response.statusCode}');
     }
   }
 }
