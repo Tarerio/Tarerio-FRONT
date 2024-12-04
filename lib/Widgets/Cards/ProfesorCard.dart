fb9ed50e8656b20eb45da7626573843bed95a0d3
Juan Miguel Acosta Ortega <acostaojuanmi@gmail.com>
2024-12-04 12:45:03 +0100

    JUANMI: Cambio referencia id_usuario

--- lib/Widgets/Cards/ProfesorCard.dart
+++ lib/Widgets/Cards/ProfesorCard.dart
@@ -93,7 +93,7 @@ class _ProfesorCardState extends State<ProfesorCard> {
                   onPressed: () {
                     Navigator.pushNamed(
                         context, '/administrador/profesores/editarProfesor',
-                        arguments: id_usuario);
+                        arguments: widget.id_usuario);
                   },
                   icon: const Icon(Icons.edit, color: Colors.teal),
                   label: const Text('Editar Profesor'),
