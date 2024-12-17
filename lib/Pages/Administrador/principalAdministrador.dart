a3066abdd930a5a44ad654f634e4ce5d850ee727
Alejandroibo <alejandroibo131313@gmail.com>
2024-12-17 01:02:43 +0100

    ALEX: Panel admin Tareas Completadas por Alumno

--- lib/Pages/Administrador/principalAdministrador.dart
+++ lib/Pages/Administrador/principalAdministrador.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tarerio/API/tareaJuegoAPI.dart';
+import 'package:tarerio/API/tareaPeticionAPI.dart';
+import 'package:tarerio/API/tareaPorPasosAPI.dart';
+import '../../API/alumnosAPI.dart';
 import '../../Widgets/Navbar.dart';
 
 class SimpleBarChart extends StatelessWidget {
@@ -46,8 +50,111 @@ class BarChartPainter extends CustomPainter {
   }
 }
 
-class AdminDashboard extends StatelessWidget {
+class AdminDashboard extends StatefulWidget {
   const AdminDashboard({super.key});
+
+  @override
+  _AdminDashboard createState() => _AdminDashboard();
+}
+
+class _AdminDashboard extends State< AdminDashboard> {
+  List<dynamic> TareasAlumnos = [];
+
+  bool isLoading = true; // Indicador de carga
+  AlumnosAPI _api = AlumnosAPI();
+
+  @override
+  void initState() {
+    super.initState();
+    _obtenerTareasCompletadas();
+  }
+
+  Future<void> _obtenerTareasCompletadas() async {
+    TareaJuegoAPI juegoAPI = TareaJuegoAPI();
+    TareaPeticionAPI peticionAPI = TareaPeticionAPI();
+    TareaPorPasosAPI porPasosAPI = TareaPorPasosAPI();
+
+    List<dynamic> Alumnos = [];
+    List<dynamic> AlumnoTarea = [];
+    try {
+      final response = await _api.getAlumnos();
+      setState(() {
+        Alumnos = response; // Actualiza la lista de tareas
+        isLoading = false; // Cambia el estado de carga
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false; // Cambia el estado de carga incluso si hay un error
+      });
+    }
+
+    List<Object> tareasCompletadasSemana = List.filled(5, 0);
+
+    DateTime hoy = DateTime.now();
+    DateTime lunesActual = hoy.subtract(Duration(days: hoy.weekday - 1));
+
+    List<DateTime> diasSemana = List.generate(5, (index) => lunesActual.add(Duration(days: index)),);
+
+    for (var alumno in Alumnos) {
+
+      int tareasCompletadas = 0;
+      int tareasJuegoCompletas = 0;
+      int tareasPasosCompletas = 0;
+      int tareasPeticionCompletas = 0;
+
+      int tareasPorHacer = 0;
+      int tareasJuegoPorHacer = 0;
+      int tareasPasosPorHacer = 0;
+      int tareasPeticionPorHacer = 0;
+
+      try {
+        final tareasJuego = await juegoAPI.obtenerAsignadasAlumno(alumno["nickname"], null, null);
+        final tareasPeticion = await peticionAPI.obtenerAsignadasAlumno(alumno["nickname"], null, null);
+        final tareasPorPasos = await porPasosAPI.obtenerAsignadasAlumno(alumno["nickname"], null, null);
+
+        for (var tarea in tareasJuego) {
+          if (tarea["completado"]){
+            tareasJuegoCompletas++;
+
+            //TODO: Comparar fecha de realizacion con dias de la semana, e incrementar lista final
+            //Haria falta modificar las tareas para añadir el campo de realizacion
+            //Deberia de ser en las tablas de realizacion
+
+          } else{
+            tareasJuegoPorHacer++;
+          }
+        }
+        for (var tarea in tareasPorPasos) {
+          if (tarea["completado"]){
+            tareasPasosCompletas++;
+          } else{
+            tareasPasosPorHacer++;
+          }
+        }
+        for (var tarea in tareasPeticion) {
+          if (tarea["completado"]){
+            tareasPeticionCompletas++;
+          } else{
+            tareasPeticionPorHacer++;
+          }
+        }
+
+        tareasCompletadas = tareasJuegoCompletas + tareasPasosCompletas + tareasPeticionCompletas;
+        tareasPorHacer = tareasJuegoPorHacer + tareasPasosPorHacer + tareasPeticionPorHacer;
+
+        var tupla = {'name' : alumno["nickname"], 'tasksDone': tareasCompletadas, 'tasksToDo' : tareasPorHacer,
+          'tasksJuegoDone' : tareasJuegoCompletas, 'tasksPasosDone' : tareasPasosCompletas, 'tasksPeticionDone' : tareasPeticionCompletas,
+          'tasksJuegoToDo' : tareasJuegoPorHacer, 'tasksPasosToDo' : tareasPasosPorHacer, 'tasksPeticionToDo' : tareasPeticionPorHacer};
+        AlumnoTarea.add(tupla);
+      } catch (e) {
+        print("Error al cargar tareas: $e");
+      }
+    }
+
+    setState(() {
+      TareasAlumnos = AlumnoTarea;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +173,9 @@ class AdminDashboard extends StatelessWidget {
             ),
             SizedBox(
               height: 200,
-              child: SimpleBarChart(data: [50, 75, 100, 150, 200]), // Add data
+              child: SimpleBarChart(
+                data: [50, 75, 100, 150, 200]
+              ), // Add data
             ),
             const SizedBox(height: 20),
             const Text(
@@ -76,7 +185,7 @@ class AdminDashboard extends StatelessWidget {
             _buildMenuList(),
             const SizedBox(height: 20),
             const Text(
-              'Ranking de Alumnos por Tareas Completadas',
+              'Tareas pendientes/completadas de los alumnos',
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold), // Larger font size
             ),
             _buildRankingList(),
@@ -114,20 +223,20 @@ class AdminDashboard extends StatelessWidget {
   }
 
   Widget _buildRankingList() {
-    final ranking = [
-      {'name': 'Juan Perez', 'tasks': 50},
-      {'name': 'Maria Lopez', 'tasks': 45},
-      {'name': 'Carlos Sanchez', 'tasks': 40},
-      {'name': 'Ana Martinez', 'tasks': 35},
-    ];
+    final ranking = TareasAlumnos;
 
     return Column(
       children: ranking.map((student) {
         return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 4.0), // Reduce vertical padding
+          padding: const EdgeInsets.symmetric(vertical: 3.0), // Reduce vertical padding
           child: ListTile(
-            title: Text(student['name'].toString()),
-            trailing: Text('${student['tasks']} tareas completadas'),
+            title: Text(student['name'].toString(), style: const TextStyle(fontSize: 20),),
+            trailing: Text('Pendientes: ${student['tasksToDo']} (Juego: ${student['tasksJuegoToDo']} | Pasos: ${student['tasksPasosToDo']} | '
+            'Peticion: ${student['tasksPeticionToDo']})   -   Completadas: ${student['tasksDone']} (Juego: ${student['tasksJuegoDone']} | '
+            'Pasos: ${student['tasksPasosDone']} | Peticion: ${student['tasksPeticionDone']})',
+              style: const TextStyle(fontSize: 16),
+            ),
+            
           ),
         );
       }).toList(),
